93b749a406240cdd6db7fb4411b078ae7fc8ac74
AlencarJr <alenior@gmail.com>
2026-05-26 16:16:55 -0300

    MQTT
    Reconexão mais robusta.

--- secrets.py
+++ secrets.py
@@ -4,7 +4,7 @@ Copie este arquivo para `secrets.py` e preencha conforme seu ambiente local.
 """
 
 # Wi-Fi
-WIFI_SSID = "Alencar's Galaxy M14 5G"
+WIFI_SSID = "GCNET-Alencar"
 WIFI_PASSWORD = "11223344"
 
 # MQTT (modo prototipação / broker público)
